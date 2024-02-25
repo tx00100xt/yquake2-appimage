45ef1d81592a0801ef58a1135b29f9f04bf2601c
Alexander Pavlov <t.x00100x.t@yandex.ru>
2024-02-25 14:10:09 +0300

    Add modifications

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #/bin/sh
 
 rm -fr yquake2
+rm -fr xatrix
+rm -fr ctf
+rm -fr rogue
+rm -fr zaero
 
 # get linuxdeploy-x86_64.AppImage
 wget -nv -c https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
@@ -13,15 +17,52 @@ git clone https://github.com/yquake2/yquake2.git
 cd yquake2
 patch -p1 < ../patches/0001-yquake2-AppImage.patch
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release ..
 make -j4
-
-# remove temp stuff
 cd ..
 cd ..
-
-# copy files
 cp -ax yquake2/build/release/* AppDir/usr/bin
+
+# xatrix
+git clone https://github.com/yquake2/xatrix.git
+cd xatrix
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+cd ..
+cd ..
+cp -f xatrix/build/Release/game.so AppDir/usr/bin/xatrix
+
+# ctf
+git clone https://github.com/yquake2/ctf.git
+cd ctf
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+cd ..
+cd ..
+cp -f ctf/build/Release/game.so AppDir/usr/bin/ctf
+
+# rogue
+git clone https://github.com/yquake2/rogue.git
+cd rogue
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+cd ..
+cd ..
+cp -f rogue/build/Release/game.so AppDir/usr/bin/rogue
+
+# zaero
+git clone https://github.com/yquake2/zaero.git
+cd zaero
+patch -p1 < ../patches/0002-zaero-AppImage.patch
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+cd ..
+cd ..
+cp -f zaero/build/Release/game.so AppDir/usr/bin/zaero
 
 # create appimage
 SIGN=1 ./linuxdeploy-x86_64.AppImage --executable AppDir/usr/bin/quake2 --desktop-file AppDir/usr/share/applications/org.yamagi.YamagiQ2.desktop --icon-file AppDir/usr/share/icons/hicolor/128x128/apps/Quake2.png --appdir AppDir --output appimage
